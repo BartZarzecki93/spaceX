39225becb282f4afc723bf87d86de97b59d170f9
Bartosz Zarzecki <bartoszzarzecki@Bartoszs-MacBook-Pro.local>
2019-04-08 21:00:17 -0400

    Heroku Finish

---
+++ public/precache-manifest.c2cf7d9c2aa389889ccf79a8b97cd910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a214b68fc4146d02a5b41882e79987c",
+    "url": "/static/media/spacex-1.9a214b68.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a734854ee9753ec0a213",
+    "url": "/static/js/main.a2ffe095.chunk.js"
+  },
+  {
+    "revision": "962b1265828958a3defd",
+    "url": "/static/js/2.5f000af4.chunk.js"
+  },
+  {
+    "revision": "a734854ee9753ec0a213",
+    "url": "/static/css/main.26db543d.chunk.css"
+  },
+  {
+    "revision": "678ebd8abdbcb08a0c7008f7ce9b9cdd",
+    "url": "/index.html"
+  }
+];
